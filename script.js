0a78f00b021e16a53f6f032cf5fd92e77f766ad0
Jill <jillannkay@gmail.com>
2020-08-10 19:47:45 +0200

    added timestamp to messages

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 console.log("Hello world!");
 
-const responses = ["Hello", "Bleh", "Goodbye", "I'm hungry", "I'm tired", "Who are you?", "Nice to meet you!", "Go away!", "You're ugly", "Zzzzzzzz"];
+const responses = ["Hello", "Bleh", "Goodbye", "I'm hungry", "I'm busy", "Who are you?", "Nice to meet you, I suppose.", "Go away!", "You're ugly", "Zzzzzzzz"];
+
+// time
+
+const timeNow = () => {
+  let date = new Date($.now());
+  let time = date.getHours() + ":" + date.getMinutes();
+  return time;
+}
+
+const displayTime = (time) => {
+  let timeStamp = $("<p>");
+  timeStamp.addClass("time-stamp").text(time);
+  return timeStamp;
+}
+
+// user input
 
 const getMessageText = () => {
   let message = $("textarea").val();
@@ -9,11 +25,17 @@ const getMessageText = () => {
 
 const createBubble = (message) => {
   let bubble = $("<div>");
-  let bubbleText = $("<p>")
+  let bubbleText = $("<p>");
   bubble.addClass("bubble bubble-right")
         .appendTo("#chat-area");
-  bubbleText.text(message).appendTo(bubble);
+  bubbleText.text(message)
+            .appendTo(bubble);
+  let timeStamp = displayTime(timeNow());
+  timeStamp.addClass("time-stamp time-stamp-right")
+           .insertAfter(bubble);
 }
+
+// chatbot response
 
 const selectChatbotAnswer = (responses) => {
   let random = Math.random() * 10;
@@ -23,23 +45,30 @@ const selectChatbotAnswer = (responses) => {
 
 const displayChatbotAnswer = (answer) => {
   let bubble = $("<div>");
-  let bubbleText = $("<p>")
+  let bubbleText = $("<p>");
   bubble.addClass("bubble bubble-left")
-        .appendTo("#chat-area").delay(800).fadeIn();
-  bubbleText.text(answer).appendTo(bubble);
+        .appendTo("#chat-area");
+  bubbleText.text(answer)
+            .appendTo(bubble);
+  let timeStamp = displayTime(timeNow());
+  timeStamp.addClass("time-stamp")
+           .insertAfter(bubble);
 }
 
+// clear
 const clearTextArea = () => {
   $("textarea").val("");
 }
 
-$("textarea").keypress(function(event) {
+// submit on enter
+$("textarea").keypress( (event) => {
   if (event.which == 13) {
     event.preventDefault();
     $("form").submit();
   }
 });
 
+// submit
 $("#form").on("submit", (event) => {
   event.preventDefault();
   createBubble(getMessageText());
