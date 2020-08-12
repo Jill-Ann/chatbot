e4c9be610014ca61c5a19b964883aca06bff827c
Jill <jillannkay@gmail.com>
2020-08-12 19:54:38 +0200

    included option to switch avatars

--- script.js
+++ script.js
@@ -101,3 +101,63 @@ $("#form").on("submit", (event) => {
   clearTextArea();
   $('#chat-area').scrollTop($('#chat-area')[0].scrollHeight);
 })
+
+// change Avatar
+
+let avatarName = $("#header-name");
+let avatarPic = $("#main-avatar-pic");
+
+$("#avatar-raffi").on("click", () => {
+  avatarName.text("Chatbot Raffi");
+  avatarPic.attr("src", "./icons/monster-6.png")
+})
+
+$("#avatar-zak").on("click", () => {
+  avatarName.text("Chatbot Zak");
+  avatarPic.attr("src", "./icons/monster-60.png")
+})
+
+$("#avatar-sergio").on("click", () => {
+  avatarName.text("Chatbot Sergio");
+  avatarPic.attr("src", "./icons/monster-2.png")
+})
+
+$("#avatar-usam").on("click", () => {
+  avatarName.text("Chatbot Usam");
+  avatarPic.attr("src", "./icons/monster-11.png")
+})
+
+$("#avatar-tommy").on("click", () => {
+  avatarName.text("Chatbot Tommy");
+  avatarPic.attr("src", "./icons/monster-46.png")
+})
+
+$("#avatar-kalaya").on("click", () => {
+  avatarName.text("Chatbot Kalaya");
+  avatarPic.attr("src", "./icons/android.png")
+})
+
+$("#avatar-linda").on("click", () => {
+  avatarName.text("Chatbot Linda");
+  avatarPic.attr("src", "./icons/monster-42.png")
+})
+
+$("#avatar-dana").on("click", () => {
+  avatarName.text("Chatbot Dana");
+  avatarPic.attr("src", "./icons/monster-1.png")
+})
+
+$("#avatar-matt").on("click", () => {
+  avatarName.text("Chatbot Matt");
+  avatarPic.attr("src", "./icons/bee.png")
+})
+
+$("#avatar-anna").on("click", () => {
+  avatarName.text("Chatbot Anna");
+  avatarPic.attr("src", "./icons/monster-41.png")
+})
+
+$("#avatar-dulce").on("click", () => {
+  avatarName.text("Chatbot Dulce");
+  avatarPic.attr("src", "./icons/monster-16.png")
+})
